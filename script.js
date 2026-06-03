5d33180d8b0c4bec9c9bcde36ff3d6d39eaed7c7
Yolanda <yolada.ocampo45@gmail.com>
2026-06-03 18:09:54 -0500

    Added music

--- script.js
+++ script.js
@@ -5,43 +5,50 @@ const curtoBt = document.querySelector(".app__card-button--curto");
 const longoBt = document.querySelector(".app__card-button--longo");
 const banner = document.querySelector(".app__image");
 const titulo = document.querySelector(".app__title");
-const botoes = document.querySelectorAll(".app__card-button")
-const displayTempo = document.querySelector("#timer");
-const musicaFocoInput = document.querySelector("#alternar-musica")
-const musica = new Audio('/sons/luna-rise-part-one.mp3') 
-musica.loop = true
+const botoes = document.querySelectorAll(".app__card-button");
+const startPauseBt = document.querySelector("#start-pause");
 
+const somBeep = new Audio("sons/beep.mp3");
+const somPlay = new Audio("sons/play.wav");
+const somPause = new Audio("sons/pause.mp3");
+
+const musicaFocoInput = document.querySelector("#alternar-musica");
+const musica = new Audio("sons/luna-rise-part-one.mp3");
+musica.loop = true;
+
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
 const duracaoFoco = 1500;
 const duracaoDescansoCurto = 300;
 const duracaoDescansoLongo = 900;
 
-musicaFocoInput.addEventListener('change', () => {
-    if (musica.paused){
-        musica.play()
-    }else{
-        musica.pause()
-    }
-})
+musicaFocoInput.addEventListener("change", () => {
+  if (musica.paused) {
+    musica.play();
+  } else {
+    musica.pause();
+  }
+});
 
 focoBt.addEventListener("click", () => {
   alterarContexto("foco");
-  focoBt.classList.add('active')
+  focoBt.classList.add("active");
 });
 
 curtoBt.addEventListener("click", () => {
   alterarContexto("descanso-curto");
-  curtoBt.classList.add('active')
+  curtoBt.classList.add("active");
 });
 
 longoBt.addEventListener("click", () => {
   alterarContexto("descanso-longo");
-  longoBt.classList.add('active')
+  longoBt.classList.add("active");
 });
 
 function alterarContexto(contexto) {
-    botoes.forEach(function(contexto){
-        contexto.classList.remove('active')
-    })
+  botoes.forEach(function (contexto) {
+    contexto.classList.remove("active");
+  });
   html.setAttribute("data-contexto", contexto);
   banner.setAttribute("src", `/imagens/${contexto}.png`);
   switch (contexto) {
@@ -63,4 +70,31 @@ function alterarContexto(contexto) {
     default:
       break;
   }
+}
+
+const contagemRegressiva = () => {
+  if (tempoDecorridoEmSegundos <= 0) {
+    somBeep.play();
+    alert("Tempo finalizado!");
+    zerar();
+    return;
+  }
+  tempoDecorridoEmSegundos -= 1;
+  console.log("Temporizador: " + tempoDecorridoEmSegundos);
+};
+
+startPauseBt.addEventListener("click", iniciarOuPausar);
+
+function iniciarOuPausar(inter) {
+  if (intervaloId) {
+    zerar();
+    somPause.play();
+    return;
+  }
+  intervaloId = setInterval(contagemRegressiva, 1000);
+  somPlay.play();
+}
+function zerar() {
+  clearInterval(intervaloId);
+  intervaloId = null;
 }
