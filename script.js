f0dad94df98b97abbb40144433eb621394360f18
Yolanda <yolada.ocampo45@gmail.com>
2026-06-03 15:03:05 -0500

    Music added

--- script.js
+++ script.js
@@ -5,44 +5,43 @@ const curtoBt = document.querySelector(".app__card-button--curto");
 const longoBt = document.querySelector(".app__card-button--longo");
 const banner = document.querySelector(".app__image");
 const titulo = document.querySelector(".app__title");
+const botoes = document.querySelectorAll(".app__card-button")
 const displayTempo = document.querySelector("#timer");
+const musicaFocoInput = document.querySelector("#alternar-musica")
+const musica = new Audio('/sons/luna-rise-part-one.mp3') 
+musica.loop = true
 
 const duracaoFoco = 1500;
 const duracaoDescansoCurto = 300;
 const duracaoDescansoLongo = 900;
 
+musicaFocoInput.addEventListener('change', () => {
+    if (musica.paused){
+        musica.play()
+    }else{
+        musica.pause()
+    }
+})
+
 focoBt.addEventListener("click", () => {
   alterarContexto("foco");
+  focoBt.classList.add('active')
 });
 
 curtoBt.addEventListener("click", () => {
   alterarContexto("descanso-curto");
+  curtoBt.classList.add('active')
 });
 
 longoBt.addEventListener("click", () => {
   alterarContexto("descanso-longo");
+  longoBt.classList.add('active')
 });
 
 function alterarContexto(contexto) {
-  html.setAttribute("data-contexto", contexto);
-  banner.setAttribute("src", `/imagens/${contexto}.png`);
-  switch (contexto) {
-    case "foco":
-      titulo.innerHTML = `Que tal dar uma respirada?<br>
-                <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
-      break;
-    case "descanse-curto":
-      titulo.innerHTML = `Otimize sua produtividade,<br>
-                <strong class="app__title-strong">mergulhe no que importa.</strong>`;
-      break;
-    case "descanse-longo":
-      titulo.innerHTML = `Hora de voltar à superfície.<br>
-                <strong class="app__title-strong">Faça uma pausa longa.</strong>`;
-    default:
-      break;
-  }
-}
-function alterarContexto(contexto) {
+    botoes.forEach(function(contexto){
+        contexto.classList.remove('active')
+    })
   html.setAttribute("data-contexto", contexto);
   banner.setAttribute("src", `/imagens/${contexto}.png`);
   switch (contexto) {
